4faf31ff13267b980659830892a40cdfa315b108
sttheplug <129483377+sttheplug@users.noreply.github.com>
2024-12-04 23:43:15 +0100

    Removed some redundant files

--- microservice-patient/src/main/java/com/example/journalsystem/db/PracitionerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.journalsystem.db;
-
-import com.example.journalsystem.bo.model.Practitioner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PracitionerRepository extends JpaRepository<Practitioner, Long> {
-}
